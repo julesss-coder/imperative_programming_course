28f9535fc38151decf6d117d35da98f481cd8708
julesss-coder <julesss@posteo.de>
2024-10-19 20:49:45 +0200

    Correct formula for meters to nautical miles.

--- main.go
+++ main.go
@@ -23,34 +23,34 @@ import (
 	"fmt"
 )
 
-func convert_meters(meters int) {
+func convert_meters(meters float64) {
 	var millimeters = meters * 1000
-	fmt.Printf("%d meters are %d millimeters.\n", meters, millimeters)
+	fmt.Printf("%.2f meters are %f millimeters.\n", meters, millimeters)
 
-	var kilometers = float64(meters) / 1000
-	fmt.Printf("%d meters are %.3f kilometers.\n", meters, kilometers)
+	var kilometers = meters / 1000
+	fmt.Printf("%.2f meters are %.3f kilometers.\n", meters, kilometers)
 
-	var inches float64 = float64(meters) / 0.0254
-	fmt.Printf("%d meters are %.2f inches.\n", meters, inches)
+	var inches float64 = meters / 0.0254
+	fmt.Printf("%.2f meters are %.2f inches.\n", meters, inches)
 
-	var sea_miles float64 = float64(meters) * 5.39956e-4
-	fmt.Printf("%d meters are %.6f seamiles.\n", meters, sea_miles)
+	var nautical_miles float64 = meters / 1852
+	fmt.Printf("%.2f meters are %.6g nautical miles.\n", meters, nautical_miles)
 
-	var light_years = float64(meters) / 9.461e15
-	fmt.Printf("%d meters are %.15f light years.\n", meters, light_years)
+	var light_years = meters / 9.461e15
+	fmt.Printf("%.2f meters are %.15g light years.\n", meters, light_years)
 }
 
 func main() {
-	var num int
+	var num float64
 	var error error
 
-	fmt.Println("Enter the number of meters (integer) you wish to convert to various formats.")
+	fmt.Println("Enter the number of meters you wish to convert to various formats.")
 	_, error = fmt.Scan(&num)
 
 	if error != nil {
 		fmt.Println("Error reading user input: ", error)
 	} else {
-		fmt.Printf("You successfully entered the integer %d. Converting to various formats now...\n", num)
+		fmt.Printf("You successfully entered the number %f. Converting to various units now...\n", num)
 		convert_meters(num)
 	}
 }
